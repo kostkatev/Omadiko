e4a6a3a9ec01922bb1c56de810de00abb3835463
thodwrhspap92@gmail.com <thodwrhspap92@gmail.com>
2021-06-25 21:22:57 +0300

    Done

--- Omadiko.RepositoryServices/CustomerRepository.cs
+++ Omadiko.RepositoryServices/CustomerRepository.cs
@@ -13,41 +13,70 @@ namespace Omadiko.RepositoryServices
     {
         ApplicationDbContext db = new ApplicationDbContext();
 
-
+        //Get All Customers
         public IEnumerable<Customer> GetAll()
         {
             return db.Customers.ToList();
         }
 
-        public IEnumerable<Customer> GetAllOrderedByName()
+        //Get All Customers Asc Order By FirstName
+        public IEnumerable<Customer> GetAllOrderedByFirstName()
         {
-            return db.Customers.OrderBy(x => x.Name).ToList();
-        }
-
-        public IEnumerable<Customer> FilterByName(string name)
-        {
-            return db.Customers.Where(x => x.Name.Contains(name)).ToList();
+            return db.Customers.OrderBy(x => x.FirstName).ToList();
         }
 
 
+        //Get All Customers Desc Order By FirstName
+        public IEnumerable<Customer> GetAllOrderedByFirstNameDesc()
+        {
+            return db.Customers.OrderByDescending(x => x.FirstName).ToList();
+        }
 
-        public Product GetById(int? id)
+        //Find Customers By FirstName
+        public IEnumerable<Customer> FilterByFIrstName(string name)
+        {
+            return db.Customers.Where(x => x.FirstName.Contains(name)).ToList();
+        }
+
+        //Get All Customers Asc Order By LastName
+        public IEnumerable<Customer> GetAllOrderedByLastName()
+        {
+            return db.Customers.OrderBy(x => x.LastName).ToList();
+        }
+
+        //Get All Customers Desc Order By LastName
+        public IEnumerable<Customer> GetAllOrderedByLastNameDesc()
+        {
+            return db.Customers.OrderByDescending(x => x.LastName).ToList();
+        }
+
+        //Find Customers By LastName
+        public IEnumerable<Customer> FilterByLastName(string name)
+        {
+            return db.Customers.Where(x => x.LastName.Contains(name)).ToList();
+        }
+
+        //Find Customers By Id
+        public Customer GetById(int? id)
         {
             return db.Customers.Find(id);
         }
 
+        //Create Customer
         public void Insert(Customer customer)
         {
             db.Entry(customer).State = EntityState.Added;
             db.SaveChanges();
         }
 
+        //Update Customer
         public void Update(Customer customer)
         {
             db.Entry(customer).State = EntityState.Modified;
             db.SaveChanges();
         }
 
+        //Delete Customer
         public void Delete(int id)
         {
             var customer = db.Customers.Find(id);
